9b3e853dd6e6bef87ea7bfddc7caf9644ae9efe9
liberuum <liberom@proton.me>
2023-06-26 15:18:48 +0100

    initiate project

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import {
+    actions,
+    BudgetStatementDocument,
+    LineItem,
+    BudgetStatement
+} from '@acaldas/document-model-libs/budget-statement';
+
+const document = new BudgetStatement()
+document.setOwner({ title: 'Sustainable Ecosystem Scaling CoreUnit', ref: "makerdao/core-unit", id: "SES-001" })
+document.addAccount([{ address: 'testAddress' }])
+
+console.log(document.owner)
+
+// const settings = { ... };
+// const assistant = new BudgetStatementAssistant(settings);
+
+/*
+- Google auth data
+- Mapping wallet address => google sheet url
+
+
+assistant class with necessary properties
+- gogle auth
+- import lineItems
+
+
+await assistant.importWalletLineItems(document, address);
+*/
+document.addLineItem('0xb5eb779ce300024edb3df9b6c007e312584f6f4f',
+    [
+        {
+            actual: 12,
+            category: { ref: 'makerdao/expense-category', id: 'Compensation&Benefits', title: 'Compensation&Benefits' },
+            group: { ref: "makerdao/budget-category", id: "makerdao/core-unit/ses", title: 'SES' },
+            forecast: [{
+                month: "2023-06",
+                budgetCap: 122,
+                value: 11
+            }]
+        },
+
+    ])
