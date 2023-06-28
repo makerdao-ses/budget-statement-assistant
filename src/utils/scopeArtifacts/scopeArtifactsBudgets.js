7a266475f14de81626b332a26ef6402116e03190
liberuum <liberom@proton.me>
2023-06-28 11:35:08 +0100

    add scopeArtifacts scripts

---
+++ src/utils/scopeArtifacts/scopeArtifactsBudgets.js
@@ -0,0 +1,125 @@
+// import knex from 'knex';
+import scopeArtifacts from './scopeArtifactsData.js';
+import fs from 'fs';
+
+
+// Connect to database selected in the .env file
+// const db = knex({
+//     client: 'pg',
+//     connection: process.env.PG_CONNECTION_STRING,
+//     idleTimeoutMillis: 0,
+// });
+
+// console.log(scopeArtifacts)
+
+/* Required values to fill in DB:
+
+Budget: 
+    - parentId
+    - name
+    - code
+    - start
+    - end
+
+BudgetCap:
+    - budgetId
+    - expenseCategoryId
+    - amount
+    - currency
+*/
+
+
+function structureData(data) {
+    let hierarchy = {};
+    let currentL0, currentL1, currentL2, currentL3, currentL4;
+
+    data.forEach(item => {
+        if (item.L0) {
+            currentL0 = item.L0;
+            hierarchy[currentL0] = item;
+        }
+        if (item.L1) {
+            currentL1 = item.L1;
+            if (!hierarchy[currentL0]) {
+                hierarchy[currentL0] = {};
+            }
+            hierarchy[currentL0][currentL1] = item;
+        }
+        if (item.L2) {
+            currentL2 = item.L2;
+            if (!hierarchy[currentL0][currentL1]) {
+                hierarchy[currentL0][currentL1] = {};
+            }
+            hierarchy[currentL0][currentL1][currentL2] = item;
+        }
+        if (item.L3) {
+            currentL3 = item.L3;
+            if (!hierarchy[currentL0][currentL1][currentL2]) {
+                hierarchy[currentL0][currentL1][currentL2] = {};
+            }
+            hierarchy[currentL0][currentL1][currentL2][currentL3] = item;
+        }
+        // if (item.L4) {
+        //     currentL4 = item.L4;
+        //     if (!hierarchy[currentL0][currentL1][currentL2][currentL3]) {
+        //         hierarchy[currentL0][currentL1][currentL2][currentL3] = {};
+        //     }
+        //     hierarchy[currentL0][currentL1][currentL2][currentL3][currentL4] = item;
+        // }
+    });
+    return hierarchy;
+}
+
+// let structuredData = structureData(scopeArtifacts);
+// structuredData
+// console.log(structuredData);
+
+// const writeToFile = (data) => {
+//     fs.writeFile('scopeArtifacts.json', JSON.stringify(data), (err) => {
+//         if (err) throw err;
+//         console.log('The file has been saved!');
+//     });
+// };
+
+// writeToFile(structuredData);
+
+
+const structuredJson = JSON.parse(fs.readFileSync('scopeArtifacts.json', 'utf8'));
+
+let budgets = [];
+let budgetCaps = [];
+
+function processObject(obj, parentId) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+            let splitKey = key.split(')');
+            let code = splitKey[0];
+            let name = splitKey[1] ? splitKey[1].trim() : key;
+            let start = obj[key]['DAI Budget Start'] || '';
+            let end = obj[key]['DAI Budget End'] || '';
+            let id = budgets.length + 1;
+
+            budgets.push({
+                id: id,
+                parentId: parentId,
+                name: name,
+                code: code,
+                start: start,
+                end: end
+            });
+
+            let amount = obj[key]['Committed Budget (DAI) New '] || 0;
+            budgetCaps.push({
+                budgetId: id,
+                amount: amount,
+                currency: 'DAI'
+            });
+
+            processObject(obj[key], id);
+        }
+    }
+}
+
+processObject(structuredJson, null);
+
+console.log(budgetCaps);
