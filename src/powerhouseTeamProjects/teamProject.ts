b98c43dcf76add266db92af8bd77f77989451631
liberuum <liberom@proton.me>
2024-08-13 16:22:08 +0100

    add contributors

--- src/powerhouseTeamProjects/teamProject.ts
+++ src/powerhouseTeamProjects/teamProject.ts
@@ -5,30 +5,32 @@ import keyResults from './keyResults.json' assert { type: "json" };
 import supportedProjects from './supportedProjects.json' assert { type: 'json' }
 import supportedKeyResults from './supportedKeyResults.json' assert { type: 'json' }
 import supportedDeliverables from './supportedDeliverables.json' assert { type: 'json' }
-
+import contributors from './contributors.json' assert { type: 'json' }
 
 const nestedProjects = (projects: any[], deliverables: any[], keyResults: any[]) => {
     projects.forEach(project => {
+        const contributor: any = contributors.find(contributor => contributor.code == project.owner);
         project.owner = {
-            id: '',
-            ref: '',
-            name: '',
-            code: project.owner,
-            imageUrl: ''
+            id: contributor?.id ?? '',
+            ref: contributor?.ref ?? '',
+            name: contributor?.name ?? '',
+            code: contributor?.code ?? '',
+            imageUrl: contributor?.imageUrl ?? ''
         }
         project.progress = {
             value: project.progress == 0 ? 0.001 : project.progress,
         }
         project.deliverables = deliverables.reduce((acc: any[], deliverable: any) => {
             if (deliverable.parentIdRef === project.id) {
+                const contributor: any = contributors.find(contributor => contributor.code == deliverable.owner);
                 acc.push({
                     ...deliverable,
                     owner: {
-                        id: '',
-                        ref: '',
-                        name: '',
-                        code: deliverable.owner,
-                        imageUrl: ''
+                        id: contributor?.id ?? '',
+                        ref: contributor?.ref ?? '',
+                        name: contributor?.name ?? '',
+                        code: contributor?.code ?? '',
+                        imageUrl: contributor?.imageUrl ?? ''
                     },
                     progress: {
                         value: deliverable.progress == 0 ? 0.001 : deliverable.progress,
@@ -49,26 +51,28 @@ const nestedProjects = (projects: any[], deliverables: any[], keyResults: any[])
 
 const nestedSupportedProjects = (supportedProjects: any[], supportedDeliverables: any[], supportedKeyResults: any[]) => {
     supportedProjects.forEach(project => {
+        const contributor: any = contributors.find(contributor => contributor.name === project.projectOwner);
         project.projectOwner = {
-            id: '',
-            ref: '',
-            name: project.projectOwner,
-            code: '',
-            imageUrl: ''
+            id: contributor?.id ?? '',
+            ref: contributor?.ref ?? '',
+            name: contributor?.name ?? '',
+            code: contributor?.code ?? '',
+            imageUrl: contributor?.imageUrl ?? ''
         }
         project.progress = {
             value: project.progress == 0 ? 0.001 : project.progress,
         }
         project.supportedDeliverables = supportedDeliverables.reduce((acc: any[], deliverable: any) => {
             if (deliverable.parentIdRef === project.id) {
+                const contributor: any = contributors.find(contributor => contributor.code === project.projectOwner);
                 acc.push({
                     ...deliverable,
                     owner: {
-                        id: '',
-                        ref: '',
-                        name: '',
-                        code: deliverable.owner,
-                        imageUrl: ''
+                        id: contributor?.id ?? '',
+                        ref: contributor?.ref ?? '',
+                        name: contributor?.name ?? '',
+                        code: contributor?.code ?? '',
+                        imageUrl: contributor?.imageUrl ?? ''
                     },
                     progress: {
                         value: deliverable.progress == 0 ? 0.001 : deliverable.progress,
