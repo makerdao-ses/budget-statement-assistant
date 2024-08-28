ccf0c95d368da0466a21f7d8d0dd6dfa04407b66
liberuum <liberom@proton.me>
2024-08-28 16:20:39 +0100

    refactor: - clean overlaping periods - add spn dimention - remove label

--- src/mip40s/mip40Budgets.ts
+++ src/mip40s/mip40Budgets.ts
@@ -1,7 +1,7 @@
 import { AnalyticsPath } from "../utils/analytics/AnalyticsPath.js"
 import { AnalyticsStore } from "../utils/analytics/AnalyticsStore.js"
 import knex from 'knex';
-import { eachMonthOfInterval, startOfMonth, endOfMonth, parseISO, addMonths, format } from "date-fns";
+import { eachMonthOfInterval, startOfMonth, isWithinInterval, isBefore, isAfter, isEqual, subDays, parseISO, addMonths, format } from "date-fns";
 
 export default class Mip40BudgetScript {
 
@@ -35,30 +35,70 @@ export default class Mip40BudgetScript {
     }
 
     private createSeries = async () => {
-        let mip40Budgets = await this.getMip40Budgets();
+        let cuMips = await this.getMip40Budgets();
+        
+        // Group cuMips by cuId
+        const groupedCuMips = cuMips.reduce((acc: any, cuMip: any) => {
+            if (!acc[cuMip.cuId]) {
+                acc[cuMip.cuId] = [];
+            }
+            acc[cuMip.cuId].push(cuMip);
+            return acc;
+        }, {});
+
+        // Fix overlapping periods for each group
+        let fixedCuMips: any[] = [];
+        for (const cuId in groupedCuMips) {
+            fixedCuMips = [...fixedCuMips, ...this.fixOverlappingPeriods(groupedCuMips[cuId], cuId)];
+        }
+
         if (this.mip40Spn) {
-            mip40Budgets = mip40Budgets.filter((mip40Budget: any) => mip40Budget.mip40Spn === this.mip40Spn);
+            fixedCuMips = fixedCuMips.filter((cuMip: any) => cuMip.mip40[0]?.mip40Spn === this.mip40Spn);
         }
 
         const series: any = [];
 
-        for (const mip40Budget of mip40Budgets) {
+        for (const cuMip of fixedCuMips) {
+            const mip40Budget = cuMip.mip40[0];
+            if (!mip40Budget) continue;
+
             const { mip40BudgetPeriod, mip40Wallet } = mip40Budget;
 
             const cuCode = await this.getCuCode(mip40Budget.mip40Spn);
             const source = AnalyticsPath.fromString(`powerhouse/legacy-api/mip40/${mip40Budget.mip40Spn}`)
 
-            // Skip if there is a budget period
-            if (mip40BudgetPeriod.length === 0) continue;
+            // Skip if there is no budget period
+            if (!mip40BudgetPeriod || mip40BudgetPeriod.length === 0) {
+                // console.log(`Skipping MIP ${mip40Budget.mip40Spn} due to missing budget period`);
+                continue;
+            }
             const { budgetPeriodStart, budgetPeriodEnd } = mip40BudgetPeriod[0];
+
+            if (!budgetPeriodStart || !budgetPeriodEnd) {
+                // console.log(`Invalid budget period for MIP ${mip40Budget.mip40Spn}: ${budgetPeriodStart} - ${budgetPeriodEnd}`);
+                continue;
+            }
+
+            const startDate = new Date(budgetPeriodStart);
+            const endDate = new Date(budgetPeriodEnd);
+
+            if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+                // console.log(`Invalid date for MIP ${mip40Budget.mip40Spn}: ${budgetPeriodStart} - ${budgetPeriodEnd}`);
+                continue;
+            }
+
+            if (isAfter(startDate, endDate)) {
+                // console.log(`Start date is after end date for MIP ${mip40Budget.mip40Spn}: ${budgetPeriodStart} - ${budgetPeriodEnd}`);
+                continue;
+            }
 
             if (mip40Wallet.length > 0) {
                 mip40Wallet.forEach((wallet: any) => {
                     // MKR Only Budget Serie
                     if (mip40Budget.mkrOnly) {
                         const mkrSerie = {
-                            start: new Date(budgetPeriodStart),
-                            end: new Date(budgetPeriodEnd),
+                            start: startDate,
+                            end: endDate,
                             source,
                             unit: 'MKR',
                             value: 0,
@@ -66,68 +106,73 @@ export default class Mip40BudgetScript {
                             dimensions: {
                                 budget: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
                                 wallet: AnalyticsPath.fromString(`atlas/${wallet.name}`),
-
+                                spn: AnalyticsPath.fromString(`atlas/legacy/${mip40Budget.mip40Spn}`),
                             },
-                            dimensionMetadata: {
-                                path: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
-                                label: mip40Budget.mip40Spn || null
-                            }
+                            // dimensionMetadata: {
+                            //     path: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
+                            //     label: mip40Budget.mip40Spn || null
+                            // }
                         }
                         series.push(mkrSerie)
                     }
                     // DAI budget serie
                     if (!mip40Budget.mkrOnly && wallet.mip40BudgetLineItem.length > 0) {
+                        try {
+                            // Get number of months in budget period
+                            const nrOfmonths = eachMonthOfInterval({ start: startDate, end: endDate }).length;
 
-                        // Get number of months in budget period
-                        const nrOfmonths = eachMonthOfInterval({ start: new Date(budgetPeriodStart), end: new Date(budgetPeriodEnd) }).length;
+                            wallet.mip40BudgetLineItem.forEach((lineItem: any) => {
+                                const totalBudget = lineItem.budgetCap * nrOfmonths;
+                                const headCount = lineItem.headcountExpense ? 'headcount' : 'non-headcount';
 
-                        wallet.mip40BudgetLineItem.forEach((lineItem: any) => {
-                            const totalBudget = lineItem.budgetCap * nrOfmonths;
-                            const headCount = lineItem.headcountExpense ? 'headcount' : 'non-headcount';
-
-                            if (nrOfmonths > 1) {
-                                const monthly = this.getListOfMonths(parseISO(budgetPeriodStart), parseISO(budgetPeriodEnd));
-                                for (let i = 0; i < nrOfmonths; i++) {
+                                if (nrOfmonths > 1) {
+                                    const monthly = this.getListOfMonths(startDate, endDate);
+                                    for (let i = 0; i < nrOfmonths; i++) {
+                                        let serie = {
+                                            start: monthly[i].periodStart,
+                                            end: monthly[i].periodEnd,
+                                            source,
+                                            value: lineItem.budgetCap,
+                                            unit: 'DAI',
+                                            metric: 'Budget',
+                                            dimensions: {
+                                                budget: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
+                                                category: AnalyticsPath.fromString(`atlas/${headCount}/mip40/${mip40Budget.mip40Spn}/${wallet.address}/${lineItem.budgetCategory}`),
+                                                wallet: AnalyticsPath.fromString(`atlas/${wallet.name}`),
+                                                spn: AnalyticsPath.fromString(`atlas/legacy/${mip40Budget.mip40Spn}`),
+                                            },
+                                            // dimensionMetadata: {
+                                            //     path: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
+                                            //     label: mip40Budget.mip40Spn || null
+                                            // }
+                                        }
+                                        series.push(serie);
+                                    }
+                                } else {
                                     let serie = {
-                                        start: monthly[i].periodStart,
-                                        end: monthly[i].periodEnd,
+                                        start: startDate,
+                                        end: endDate,
                                         source,
-                                        value: lineItem.budgetCap,
+                                        value: totalBudget,
                                         unit: 'DAI',
                                         metric: 'Budget',
                                         dimensions: {
                                             budget: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
                                             category: AnalyticsPath.fromString(`atlas/${headCount}/mip40/${mip40Budget.mip40Spn}/${wallet.address}/${lineItem.budgetCategory}`),
                                             wallet: AnalyticsPath.fromString(`atlas/${wallet.name}`),
+                                            spn: AnalyticsPath.fromString(`atlas/legacy/${mip40Budget.mip40Spn}`),
                                         },
-                                        dimensionMetadata: {
-                                            path: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
-                                            label: mip40Budget.mip40Spn || null
-                                        }
+                                        // dimensionMetadata: {
+                                        //     path: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
+                                        //     label: mip40Budget.mip40Spn || null
+                                        // }
                                     }
                                     series.push(serie);
                                 }
-                            } else {
-                                let serie = {
-                                    start: new Date(budgetPeriodStart),
-                                    end: new Date(budgetPeriodEnd),
-                                    source,
-                                    value: budgetPeriodStart !== null && budgetPeriodEnd !== null ? totalBudget : lineItem.budgetCap,
-                                    unit: 'DAI',
-                                    metric: 'Budget',
-                                    dimensions: {
-                                        budget: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
-                                        category: AnalyticsPath.fromString(`atlas/${headCount}/mip40/${mip40Budget.mip40Spn}/${wallet.address}/${lineItem.budgetCategory}`),
-                                        wallet: AnalyticsPath.fromString(`atlas/${wallet.name}`),
-                                    },
-                                    dimensionMetadata: {
-                                        path: AnalyticsPath.fromString(`atlas/legacy/core-units/${cuCode}`),
-                                        label: mip40Budget.mip40Spn || null
-                                    }
-                                }
-                                series.push(serie);
-                            }
-                        })
+                            })
+                        } catch (error) {
+                            console.error(`Error processing MIP ${mip40Budget.mip40Spn}:`, error);
+                        }
                     }
                 });
             }
@@ -136,42 +181,104 @@ export default class Mip40BudgetScript {
         return series;
     }
 
+    private fixOverlappingPeriods = (cuMips: any, cuId: string) => {
+        // Separate MKR-only and non-MKR-only MIPs
+        const mkrOnlyMips = cuMips.filter((cuMip: any) =>
+            cuMip.cuId === cuId &&
+            cuMip.mip40 &&
+            cuMip.mip40.length > 0 &&
+            cuMip.mip40[0].mkrOnly
+        );
+
+        const nonMkrOnlyMips = cuMips.filter((cuMip: any) =>
+            cuMip.cuId === cuId &&
+            cuMip.mip40 &&
+            cuMip.mip40.length > 0 &&
+            !cuMip.mip40[0].mkrOnly
+        );
+
+        // Function to sort and fix overlaps
+        const sortAndFixOverlaps = (mips: any[]) => {
+            mips.sort((a: any, b: any) => {
+                const aStart = a.mip40[0]?.mip40BudgetPeriod?.[0]?.budgetPeriodStart;
+                const bStart = b.mip40[0]?.mip40BudgetPeriod?.[0]?.budgetPeriodStart;
+                if (!aStart || !bStart) return 0;
+                return new Date(aStart).getTime() - new Date(bStart).getTime();
+            });
+
+            for (let i = 0; i < mips.length - 1; i++) {
+                const currentMip = mips[i].mip40[0];
+                const nextMip = mips[i + 1].mip40[0];
+
+                if (!currentMip?.mip40BudgetPeriod?.[0] || !nextMip?.mip40BudgetPeriod?.[0]) continue;
+
+                const currentEnd = new Date(currentMip.mip40BudgetPeriod[0].budgetPeriodEnd);
+                const nextStart = new Date(nextMip.mip40BudgetPeriod[0].budgetPeriodStart);
+
+                if (isAfter(currentEnd, nextStart) || isEqual(currentEnd, nextStart)) {
+                    currentMip.mip40BudgetPeriod[0].budgetPeriodEnd = subDays(nextStart, 1).toISOString().split('T')[0];
+                    // console.log(`Overlap fixed for ${currentMip.mkrOnly ? 'MKR-only' : 'non-MKR-only'} MIP ${currentMip.mip40Spn} and ${nextMip.mip40Spn}`);
+                }
+            }
+
+            return mips;
+        };
+
+        // Fix overlaps for MKR-only and non-MKR-only MIPs separately
+        const fixedMkrOnlyMips = sortAndFixOverlaps(mkrOnlyMips);
+        const fixedNonMkrOnlyMips = sortAndFixOverlaps(nonMkrOnlyMips);
+
+        // Log the fixed periods
+        [...fixedMkrOnlyMips, ...fixedNonMkrOnlyMips].forEach((mip: any) => {
+            const { mip40BudgetPeriod, mip40Spn, mkrOnly } = mip.mip40[0];
+            if (mip40BudgetPeriod && mip40BudgetPeriod[0]) {
+                // console.log(`${mkrOnly ? 'MKR-only' : 'Non-MKR-only'} ${mip40Spn}: ${mip40BudgetPeriod[0].budgetPeriodStart} - ${mip40BudgetPeriod[0].budgetPeriodEnd}`);
+            }
+        });
+
+        // Return the combined fixed MIPs
+        return [...fixedMkrOnlyMips, ...fixedNonMkrOnlyMips];
+    }
 
     private getMip40Budgets = async () => {
         const query = `
-            query Mip40s {
-                mip40s {
-                id
-                cuMipId
-                mip40Spn
-                mkrOnly
-                mkrProgramLength
-                mip40BudgetPeriod {
+            query CuMips {
+                cuMips {
                     id
-                    mip40Id
-                    budgetPeriodStart
-                    budgetPeriodEnd
-                    ftes
-                }
-                mip40Wallet {
-                    id
-                    mip40Id
-                    address
-                    name
-                    signersTotal
-                    signersRequired
-                    clawbackLimit
-                    mip40BudgetLineItem {
-                    id
-                    mip40WalletId
-                    position
-                    budgetCategory
-                    budgetCap
-                    canonicalBudgetCategory
-                    group
-                    headcountExpense
+                    cuId
+                    mip40 {
+                        id
+                        cuMipId
+                        mip40Spn
+                        mkrOnly
+                        mkrProgramLength
+                        mip40BudgetPeriod {
+                            id
+                            mip40Id
+                            budgetPeriodStart
+                            budgetPeriodEnd
+                            ftes
+                        }
+                        mip40Wallet {
+                            id
+                            mip40Id
+                            address
+                            name
+                            signersTotal
+                            signersRequired
+                            clawbackLimit
+                            mip40BudgetLineItem {
+                                id
+                                mip40WalletId
+                                position
+                                budgetCategory
+                                budgetCap
+                                canonicalBudgetCategory
+                                group
+                                headcountExpense
+                            }
+                        }
                     }
-                }
                 }
             }
         `;
@@ -185,7 +292,7 @@ export default class Mip40BudgetScript {
                 body: JSON.stringify({ query }),
             });
             const data = await response.json();
-            return data.data.mip40s;
+            return data.data.cuMips;
         } catch (error) {
             console.error(error)
         }
